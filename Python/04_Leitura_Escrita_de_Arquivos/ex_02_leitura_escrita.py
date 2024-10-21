70edb919be6be5140262b76342cb92418dd45754
Ariel Riello <riello.programmer@gmail.com>
2024-10-21 10:51:24 -0300

    Estudos Python Data Engineer

--- Python/04_Leitura_Escrita_de_Arquivos/ex_02_leitura_escrita.py
+++ Python/04_Leitura_Escrita_de_Arquivos/ex_02_leitura_escrita.py
@@ -6,3 +6,54 @@ Exercício 2: Manipulação de Arquivo CSV
 
 """
 
+import csv
+
+def criar_arquivo():
+    dados = [
+        ["Nome", "Idade"],
+        ["Ariel", 32],
+        ["João", 16],
+        ["Ana", 18],
+        ["Carlos", 17],
+    ]
+
+    nome_arquivo = "dados.csv"
+
+    with open(nome_arquivo, mode="w", newline="") as arquivo_csv:
+        escritor = csv.writer(arquivo_csv)
+        escritor.writerows(dados)
+
+    print(f"Arquivo {nome_arquivo} criado com sucesso!")
+
+
+
+def ler_arquivo(local_arquivo):
+    with open(local_arquivo, "r") as arquivo:
+        
+        conteudo = arquivo.read()
+        
+        print(conteudo)
+
+
+def filtrar_maior_idade(local_arquivo):
+    maiores = []
+
+    with open(local_aquivo, "r") as arquivo:
+        leitor = csv.reader(arquivo)
+        next(leitor)
+
+        for linha in leitor:
+            nome, idade = linha
+            if int(idade) >= 18:
+                maiores.append([nome, idade])
+
+        print(maiores)
+
+
+local_aquivo = "Python/04_Leitura_Escrita_de_Arquivos/dados.csv"
+
+criar_arquivo(local_aquivo)
+
+ler_arquivo(local_aquivo)
+
+filtrar_maior_idade(local_aquivo)
